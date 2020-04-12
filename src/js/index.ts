984c2f157e566bd57e1c28bf1c38820e8533873f
Anders Børjesson <anbo@zealand.dk>
2020-04-12 18:32:06 +0200

    changes

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,41 @@
+//import Vue from 'vue'
+
 interface Person {
     firstName: string;
     lastName: string;
 }
+let user: Person = { firstName: "Anders", lastName: "B" };
 
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!', // string
+        name: 'Anders',
+        prime: 14593, // number
+        weekdays: ['Monday', 'Tuesday', 'Wednesday'], // array of strings
+        person: { firstName: "John", lastName: "Doe" }, // object
+        teachers: [user, { firstName: "Jamshid", lastName: "E" }, { firstName: "Michael", lastName: "H" }],
+        counter: 0,
+        anotherMessage: "",
+        checked: false,
+        age: 0
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('');
+            this.prime = 11
+        },
+        updateCounter: function () {
+            this.counter++
+        }
+    }
+})
+
+/*
 function greeter(person: Person): string {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let element: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
 element.innerHTML = greeter(user);
+*/
