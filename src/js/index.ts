eda75bd7fc014a5def98997ece0692e687a69023
Anders Børjesson <anbo@zealand.dk>
2020-09-16 13:01:55 +0200

    clean up

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,7 +2,7 @@ interface Person {
     firstName: string;
     lastName: string;
 }
-let user: Person = { firstName: "Anders", lastName: "B" };
+let person1: Person = { firstName: "Anders", lastName: "B" }; 
 
 var app = new Vue({
     el: '#app',
@@ -11,24 +11,23 @@ var app = new Vue({
         name: 'Anders',
         prime: 14593, // number
         weekdays: ['Monday', 'Tuesday', 'Wednesday'], // array of strings
-        person: { firstName: "John", lastName: "Doe" }, // object
-        teachers: [user, { firstName: "Jamshid", lastName: "E" }, { firstName: "Michael", lastName: "H" }],
+        person2: { firstName: "John", lastName: "Doe" }, // object
+        teachers: [person1, { firstName: "Jamshid", lastName: "E" }, { firstName: "Michael", lastName: "H" }],
         counter: 0,
         myNumber: 0,
         inputMessage: "",
         outputMessage: "",
         anotherMessage: "",
-        checked: false,
-        age: 0
+        checked: false
     },
     methods: {
-        reverse() { // side effects
+        reverse(): void { // side effects
             this.outputMessage = this.inputMessage.split('').reverse().join('');
         },
         reverseProperFunction(m: string) { // proper function, no side effects
             return m.split('').reverse().join('');
         },
-        updateCounter() { // side effects
+        updateCounter(): void { // side effects
             this.counter++
         },
         next(n: number): number {
